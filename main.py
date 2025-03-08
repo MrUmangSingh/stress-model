f0670b72b2693b14c0dc028b9813ae690a62220c
MrUmangSingh <145918435+MrUmangSingh@users.noreply.github.com>
2025-03-08 18:14:48 +0530

    Updated all essential functions to get output

--- main.py
+++ main.py
@@ -3,5 +3,24 @@ import numpy as np
 import cv2
 
 
-model = tf.keras.models.load_model("stress_detector.h5", compile=False)
-model.summary()
+interpreter = tf.lite.Interpreter(model_path="stress_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+image_path = "test.jpg"
+img = cv2.imread(image_path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (224, 224))
+img = img.astype(np.float32) / 255.0
+img = np.expand_dims(img, axis=0)
+
+interpreter.set_tensor(input_details[0]['index'], img)
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+predicted_class = np.argmax(output_data)
+
+print("Predicted class: ", predicted_class)
+print("Output data: ", output_data)
